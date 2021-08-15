95aaca6a391be802abeacd8f4f25445b595c8322
filatovnikita27 <filatovnikita27@gmail.com>
2021-08-15 17:40:47 +0300

    fix

--- src/modules/Category/router.ts
+++ src/modules/Category/router.ts
@@ -4,11 +4,13 @@ import { getCategory } from "./controllers/getCategory";
 import { getCategoryById } from "./controllers/getCategoryById";
 import { deleteProduct } from "../Products/controllers/deleteProduct";
 import { updateProduct } from "../Products/controllers/updateProduct";
+import { deleteCategory } from "./controllers/deleteCategory";
+import { updateCategory } from "./controllers/updateCategory";
 
 export const CategoryRouter = (router: Router) => {
 	router.post("/category/post", postCategory);
 	router.get("/category/get_all", getCategory);
 	router.get("/category/get_one/:id", getCategoryById);
-	router.delete("/category/delete/:id", deleteProduct);
-	router.put("/category/update/:id", updateProduct);
+	router.delete("/category/delete/:id", deleteCategory);
+	router.put("/category/update/:id", updateCategory);
 };
