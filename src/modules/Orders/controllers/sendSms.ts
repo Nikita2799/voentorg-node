7897326e1972ded63337db6aa179948deaa954e2
filatovnikita27 <filatovnikita27@gmail.com>
2021-10-07 14:51:13 +0300

    la

--- src/modules/Orders/controllers/sendSms.ts
+++ src/modules/Orders/controllers/sendSms.ts
@@ -22,7 +22,7 @@ export const smsSend = async (req: Request, res: Response) => {
 const initSendpulse = async (phone: string, message: string) => {
 	const tokenAccess = await getAccessToken();
 	//console.log(tokenAccess?.access_token);
-	const date = moment().add(1, "seconds").format("Y-M-D h:mm:ss");
+	const date = moment().add(1, "seconds").format("Y-M-D kk:mm:ss");
 	console.log(date);
 
 	sendpulse.init(
