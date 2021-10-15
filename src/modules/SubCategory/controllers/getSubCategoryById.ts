062b9372a726b0a53b126ab05dcf008b501a678f
filatovnikita27 <filatovnikita27@gmail.com>
2021-10-15 14:05:31 +0300

    dsa

--- src/modules/SubCategory/controllers/getSubCategoryById.ts
+++ src/modules/SubCategory/controllers/getSubCategoryById.ts
@@ -6,7 +6,7 @@ const db: DatabaseApi = new DatabaseApi();
 export const getSubCategoryById = async (req: Request, res: Response) => {
 	try {
 		const { id } = req.params;
-		const params = ["subcategory", "engName", id];
+		const params = ["subcategory", "categoryId", id];
 		let categoryList: Array<unknown> = await db.category.getOneById(params);
 
 		res.status(200).json(categoryList[0]);
